7ae463c56f68d5828e9a6f85da111c3651d9d19c
Alexander <ezyyk77@gmail.com>
2019-11-10 14:27:33 +0200

    Pathfinder

--- Pathfinder/inc/pathfinder.h
+++ Pathfinder/inc/pathfinder.h
@@ -1,6 +1,8 @@
 #ifndef PATHFINDER_HEADER
 #define PATHFINDER_HEADER
 
+#define INF 999999999
+
 #include "libmx.h"
 
 typedef enum e_error
@@ -13,9 +15,17 @@ typedef enum e_error
     INVLD_NUM_ISLDS
 } t_error;
 
+typedef struct s_edge
+{
+    char *island1;
+    char *island2;
+    int weight;
+} t_edge;
+
 void mx_printerr_pf(t_error err, const char *s);
 bool mx_isnumber(char *s);
 int mx_arrlen(char **arr);
 int mx_atoi(const char *str);
+char **mx_validate_file(const char *filename);
 
 #endif
